868c5f24dcb4d7a01b2793465805d1a4a2d8a582
Rey Mercado <reymercado.usa@gmail.com>
2020-01-03 15:47:54 -0800

    first three exercises complete of required  27 exercises for ReactiveX

---
+++ solutions1-27/01.js
@@ -0,0 +1,19 @@
+function one() {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+		counter;
+
+	for(counter = 0; counter < names.length; counter++) {
+		console.log(names[counter]);
+	}
+}
+one();
+
+//above is code that can be run from console.  below in block comment is what can be run in the index.html
+/* function(console) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"],
+		counter;
+
+	for(counter = 0; counter < names.length; counter++) {
+		console.log(names[counter]);
+	}
+} */
---
+++ solutions1-27/02.js
@@ -0,0 +1,18 @@
+function two() {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+	names.forEach(function(name) {
+		console.log(name);
+	});
+}
+two();
+
+//above is code that can be run from console.  below in block comment is what can be run in the index.html
+/* function(console) {
+	var names = ["Ben", "Jafar", "Matt", "Priya", "Brian"];
+
+	names.forEach(function(name) {
+		console.log(name);
+	});
+}
+		 */
